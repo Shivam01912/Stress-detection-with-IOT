cb66b535bf3fd6c5ce5d307ce97f3b11441b5f40
Shivam01912 <shivam01912@gmail.com>
2018-07-16 17:55:10 +0530

    zero division error corrected

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -18,8 +18,6 @@ def getText():
 
 text=getText()
 
-
-
 #--------------------------------------Loading hedonometer word dataset-------------------------------------------
 tempHedo=open('hedonometer.json')  
 tempHedo=json.load(tempHedo)
@@ -29,8 +27,6 @@ for p in tempHedo['objects']:
 	hedonometerWordDict[p['word']]=(p['happs']-1)*(10/(8))-5	#Normalisation of data
 
 del tempHedo
-
-
 
 #-------------------------------------Calculating average happs score----------------------------------------------
 happsSum=0
@@ -51,40 +47,40 @@ for w in text:
 		yAxis.append(happsSum/textLen)
 	except (KeyError):
 		pass
-
-avgStress=happsSum/textLen
-
-print "Final score :",avgStress
-print "Min score :",min(yAxis)
-
-
+try:
+	avgStress=happsSum/textLen
+	print "Final score :",avgStress
+	print "Min score :",min(yAxis)
 
 #------------------------------------------------Graph plotting-------------------------------------------------------
-xAxis=range(-1,textLen+2)
-avg=[]
-medium=[]
-high=[]
-critical=[]
-veryCritical=[]
-for x in xAxis:									#Creating reference lines
-	avg.append(avgStress)
-	medium.append(0.3125)
-	high.append(-0.625)
-	critical.append(-1.25)
-	veryCritical.append(-1.875)
+	xAxis=range(-1,textLen+2)
+	avg=[]
+	medium=[]
+	high=[]
+	critical=[]
+	veryCritical=[]
+	for x in xAxis:									#Creating reference lines
+		avg.append(avgStress)
+		medium.append(0.3125)
+		high.append(-0.625)
+		critical.append(-1.25)
+		veryCritical.append(-1.875)
 
-plt.plot(range(textLen), yAxis,linewidth=2.0)
+	plt.plot(range(textLen), yAxis,linewidth=2.0)
 
-plt.plot(xAxis, avg,'k:', label="average stress score")
-plt.plot(xAxis, medium,'g--', label="medium stress")
-plt.plot(xAxis, high,'y--', label="high stress")
-plt.plot(xAxis, critical,'m--', label="critical stress")
-plt.plot(xAxis, veryCritical,'r--', label="very critical stress")
+	plt.plot(xAxis, avg,'k:', label="average stress score")
+	plt.plot(xAxis, medium,'g--', label="medium stress")
+	plt.plot(xAxis, high,'y--', label="high stress")
+	plt.plot(xAxis, critical,'m--', label="critical stress")
+	plt.plot(xAxis, veryCritical,'r--', label="very critical stress")
 
-plt.legend()
+	plt.legend()
 
-plt.xlabel('Number of Words')
-plt.ylabel('Stress level')
-plt.axis([-1, textLen+1, min(yAxis)-1, max(yAxis)+1])
+	plt.xlabel('Number of Words')
+	plt.ylabel('Stress level')
+	plt.axis([-1, textLen+1, min(yAxis)-1, max(yAxis)+1])
 
-plt.show()
+	plt.show()
+
+except (ZeroDivisionError):
+	print "Text Length Invalid"
